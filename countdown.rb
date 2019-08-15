5fa50fadc5659411d82075bd05bf09d39d8e0650
Roberto Medina <robby.medina562@gmail.com>
2019-08-15 06:37:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
-#write your code here
+def countdown(counter)
+  counter = 5
+  until counter == 0
+  puts "#{counter} SECONDS"
+  counter -= 1
+  return "HAPPY NEW YEAR!"
+end
+end
 
-def countdown
+def countdown_with_sleep(count)
+  count = 5
+  until count == 0
+  puts " #{count} SECONDS"
+  sleep 1
+end
 end
