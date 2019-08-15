36bc22719f4daf37d9e8c42590bb7ed8e9062316
Roberto Medina <robby.medina562@gmail.com>
2019-08-15 06:43:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 def countdown(seconds)
   seconds = 5
-  while counter > 0
-  puts "#{counter} SECONDS"
-  counter -= 1
+  while seconds >= 0
+  puts "#{seconds} SECONDS"
+  seconds -= 1
   return "HAPPY NEW YEAR!"
 end
 end
